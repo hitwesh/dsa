cb0f7083fb7904b3322a328c0bcc4c79007143bb
hitweshh <hiteshroy0001@gmail.com>
2026-08-17 12:38:05 +0530

    Add script to count the number of equal elements in two arrays

---
+++ cognizant/7.py
@@ -0,0 +1,8 @@
+#Counting Elements in Two Arrays: Count the number of elements in two arrays that are equal.
+a = list(map(int, input("Enter the array: ").split()))
+b = list(map(int, input("Enter the array: ").split()))
+count = 0
+for i in range(0, len(a)-1):
+    if a[i] in b:
+        count+=1
+print(count)
