21bdf1cb317b2b3ee7f62fbaac6d5ce31d059927
hitweshh <hiteshroy0001@gmail.com>
2026-09-19 21:27:22 +0530

    Implement insertion sort algorithm

--- more/sorting/2.py
+++ more/sorting/2.py
@@ -1,1 +1,10 @@
-#Implement selection sort.
+#Implement insertion sort.
+a = list(map(int, input("Input an unsorted array: ").split()))
+n = len(a)
+for i in range(1,n):
+    for j in range(i, 0, -1):
+        if a[j-1]>a[j]:
+            a[j-1], a[j] = a[j], a[j-1]
+        else:
+            break
+print(a)
